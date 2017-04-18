f6423b1855ba77cd7c5b09a0e55b37b123154121
Kai Uwe Broulik <kde@privat.broulik.de>
2017-04-18 15:39:34 +0200

    Add settings page to enable/disable plugins and some about data
    Also introduce a constants.js which will contain constants that we will use
    in different parts (e.g. options.js needs the default settings and so does extensions.js)

---
+++ extension/constants.js
@@ -0,0 +1,21 @@
+
+DEFAULT_EXTENSION_SETTINGS = {
+    mpris: {
+        enabled: true
+    },
+    kdeconnect: {
+        enabled: true
+    },
+    downloads: {
+        enabled: true
+    },
+    tabsrunner: {
+        enabled: true
+    },
+    slc: {
+        enabled: true
+    },
+    incognito: {
+        enabled: false
+    }
+};
---
+++ extension/options.js
@@ -0,0 +1,133 @@
+/*
+    Copyright (C) 2017 Kai Uwe Broulik <kde@privat.broulik.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ */
+
+function tabClicked(tabbar, tabbutton) {
+    tabbar.buttons.forEach(function (button) {
+        var tablink = button.dataset.tabLink
+
+        var tabtarget = document.querySelector("[data-tab-id=" + tablink + "]");
+
+        if (tabbutton == button) {
+            button.classList.add("active");
+            tabtarget.classList.add("active");
+        } else {
+            button.classList.remove("active");
+            tabtarget.classList.remove("active");
+        }
+    });
+}
+
+function extensionCheckboxes() {
+    return document.querySelectorAll("#extensions-selection input[type=checkbox][data-extension]");
+}
+
+function loadSettings() {
+    chrome.storage.sync.get(DEFAULT_EXTENSION_SETTINGS, function (items) {
+        if (chrome.runtime.lastError) {
+            return cb(chrome.runtime.lastError);
+        }
+
+        for (var key in items) {
+            if (!items.hasOwnProperty(key)) {
+                continue;
+            }
+
+            var checkbox = document.querySelector("input[type=checkbox][data-extension=" + key + "]");
+            if (!checkbox) {
+                console.warn("Failed to find checkbox for extension", key);
+                continue;
+            }
+
+            var checked = !!items[key].enabled;
+            checkbox.checked = checked;
+
+            // TODO restore additional stuff if we have it
+        }
+    });
+}
+
+function saveSettings(cb) {
+    var settings = {};
+
+    for (var key in DEFAULT_EXTENSION_SETTINGS) {
+        if (!DEFAULT_EXTENSION_SETTINGS.hasOwnProperty(key)) {
+            continue;
+        }
+
+        var checkbox = document.querySelector("input[type=checkbox][data-extension=" + key + "]");
+        if (!checkbox) {
+            console.warn("Failed to find checkbox for extension", key);
+            continue;
+        }
+
+        settings[key] = {
+            enabled: checkbox.checked
+        };
+
+        // TODO save additional stuff if we have it
+    }
+
+    chrome.storage.sync.set(settings, function () {
+        return cb(chrome.runtime.lastError);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // poor man's tab widget :)
+    document.querySelectorAll(".tabbar").forEach(function (tabbar) {
+        tabbar.buttons = [];
+
+        tabbar.querySelectorAll("[data-tab-link]").forEach(function (button) {
+
+            var tablink = button.dataset.tabLink
+
+            var tabtarget = document.querySelector("[data-tab-id=" + tablink + "]");
+
+            button.addEventListener("click", function () {
+                tabClicked(tabbar, button);
+            });
+
+            tabbar.buttons.push(button);
+
+            // start with the one tab page that is active
+            if (tabtarget.classList.contains("active")) {
+                tabClicked(tabbar, button);
+            }
+        });
+    });
+
+    loadSettings();
+
+    document.getElementById("save").addEventListener("click", function () {
+        var saveMessage = document.getElementById("save-message");
+        saveMessage.innerText = "";
+
+        saveSettings(function (error) {
+            if (error) {
+                saveMessage.innerText = "Saving settings failed";
+            } else {
+                saveMessage.innerText = "Settings successfully saved";
+            }
+        });
+    });
+
+    document.getElementById("clear-settings").addEventListener("click", function () {
+        chrome.storage.sync.clear(function () {
+            if (chrome.runtime.lastError) {
+                console.warn("Clearing settings failed", chrome.runtime.lastError);
+                return;
+            }
+
+            console.log("Settings cleared");
+            loadSettings();
+        });
+    });
+
+});
