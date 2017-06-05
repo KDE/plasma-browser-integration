3649aa7efbfa83b3b2fee3ef8562869fefdf4085
David Edmundson <kde@davidedmundson.co.uk>
2017-06-05 15:36:23 +0100

    Fix lie in Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -3,8 +3,6 @@
 # This script requires
 # https://github.com/i18next/i18next-gettext-converter
 
-# This is deliberately renamed out the way so we don't break scripty till we talk to i18n/sysadmin
-
 # We fill in the en "translations" manually. We extract this to the KDE system as pot as normal
 # then on release, download them all and run i18next-conv to turn them all back into JSON in all the languages
 
