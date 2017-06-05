4768d7352bcf2e13a69ab1a763af6af13c16d495
David Edmundson <kde@davidedmundson.co.uk>
2017-06-05 15:43:29 +0100

    Fix path in Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -6,4 +6,6 @@
 # We fill in the en "translations" manually. We extract this to the KDE system as pot as normal
 # then on release, download them all and run i18next-conv to turn them all back into JSON in all the languages
 
-i18next-conv -l en -s ./_locales/en/messages.json -t $podir/plasma-browser-extension.pot
+# This is outside the extension folder so it can be zipped upped and deployed without build system stuff
+
+i18next-conv -l en -s ./extension/_locales/en/messages.json -t $podir/plasma-browser-extension.pot
