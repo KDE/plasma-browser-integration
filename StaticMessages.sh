fc5dc0225d71aa1cebb6a0e2e820d9e97b1f109d
David Edmundson <kde@davidedmundson.co.uk>
2017-06-09 13:52:41 +0100

    Replace Messages.sh with version that replaces translations

---
+++ StaticMessages.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# This script requires
+# https://github.com/i18next/i18next-gettext-converter
+
+# We fill in the en "translations" manually. We extract this to the KDE system as pot as normal, then populate the other json files
+
+
+# The name of catalog we create (without the.pot extension), sourced from the scripty scripts
+FILENAME="plasma-browser-extension"
+
+function export_pot_file # First parameter will be the path of the pot file we have to create, includes $FILENAME
+{
+    potfile=$1
+    i18next-conv -l en -s ./extension/_locales/en/messages.json -t $potfile
+}
+
+function import_po_files # First parameter will be a path that will contain several .po files with the format LANG.po
+{
+    podir=$1
+    for file in `ls $podir`
+    do
+        lang=${file%.po} #remove .po from end of file
+        i18next-conv -l $lang -s $podir/$file -t ./extension/_locales/$lang/messages.json
+    done
+}
+
