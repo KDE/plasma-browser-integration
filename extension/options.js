c251703182e7bd8d3d066008504c8a8374100ebd
Jonathan Riddell <jr@jriddell.org>
2017-06-21 17:39:19 +0100

    tidy up licence headers and make it all GPL 3 for consistency

--- extension/options.js
+++ extension/options.js
@@ -1,10 +1,18 @@
 /*
     Copyright (C) 2017 Kai Uwe Broulik <kde@privat.broulik.de>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 3 of
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var storage = (IS_FIREFOX ? chrome.storage.local : chrome.storage.sync);
