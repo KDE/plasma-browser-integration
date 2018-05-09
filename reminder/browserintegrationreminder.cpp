2723ef0c80a676bf72e1e8d641a7cab806465e67
David Edmundson <kde@davidedmundson.co.uk>
2018-05-09 16:44:40 +0900

    WIP: Show SNI to install browser-side extension
    Summary:
    PBI comes as two parts, the host and the browser extension which come from different sources.
    
    This daemon notifies the user when the has the host installed, but is yet to install the browser side extension a "broken", but likely setup. It then provides a link to the relevant browser store.
    
    In an attempt to not be too annoying this only appears a few times then removes itself.
    
    I'm away next week, Kai can you commandeer - I don't like the module names.
    
    Reviewers: davidedmundson, apol, mart, #plasma
    
    Subscribers: alexeymin, zzag, hein, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D12698

---
+++ reminder/browserintegrationreminder.cpp
@@ -0,0 +1,191 @@
+/***************************************************************************
+ *   Copyright 2018 by David Edmundson <davidedmundson@kde.org>            *
+ *   Copyright 2018 by Eike Hein <hein@kde.org>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#include "browserintegrationreminder.h"
+
+#include <QDBusConnection>
+#include <QDBusServiceWatcher>
+#include <QDebug>
+#include <QDBusMessage>
+#include <QDesktopServices>
+#include <QTimer>
+#include <QDBusConnectionInterface>
+#include <QMenu>
+#include <QAction>
+
+#include <KActivities/ResourceInstance>
+#include <KConfig>
+#include <KConfigGroup>
+#include <KLocalizedString>
+#include <kpluginfactory.h>
+#include <KRun>
+#include <KService>
+#include <KSharedConfig>
+#include <KStartupInfo>
+#include <KStatusNotifierItem>
+
+#include "browserintegrationreminder.h"
+
+K_PLUGIN_FACTORY_WITH_JSON(BrowserIntegrationReminderFactory,
+                           "browserintegrationreminder.json",
+                           registerPlugin<BrowserIntegrationReminder>();)
+
+#define MAX_SHOW_COUNT 3
+
+BrowserIntegrationReminder::BrowserIntegrationReminder(QObject *parent, const QList<QVariant>&)
+      : KDEDModule(parent)
+{
+    m_debug = qEnvironmentVariableIsSet("PLASMA_BROWSE_REMIND_FORCE");
+    auto config = KSharedConfig::openConfig()->group("PlasmaBrowserIntegration");
+    m_shownCount = config.readEntry("shownCount", 0);
+
+    if (m_shownCount > MAX_SHOW_COUNT && !m_debug) {
+        disableAutoload(); //safer than it looks it won't be processed till we hit the event loop
+        return;
+    }
+
+    QUrl firefox("https://addons.mozilla.org/en-US/firefox/addon/plasma-integration/");
+    m_browsers["firefox.desktop"] = firefox;
+    m_browsers["nightly.desktop"] = firefox;
+
+    QUrl chrome("https://chrome.google.com/webstore/detail/plasma-integration/cimiefiiaegbelhefglklhhakcgmhkai");
+    m_browsers["google-chrome.desktop"] = chrome;
+    m_browsers["google-chrome-beta.desktop"] = chrome;
+    m_browsers["google-chrome-unstable.desktop"] = chrome;
+    m_browsers["chromium-browser.desktop"] = chrome;
+
+    setModuleName(QStringLiteral("BrowserIntegrationReminder"));
+    QDBusConnection dbus = QDBusConnection::sessionBus();
+    dbus.connect(QStringLiteral("org.kde.ActivityManager"),
+                QStringLiteral("/ActivityManager/Resources/Scoring"),
+                QStringLiteral("org.kde.ActivityManager.ResourcesScoring"),
+                QStringLiteral("ResourceScoreUpdated"),
+                this,
+                SLOT(onResourceScoresChanged(QString,QString,QString,double,unsigned int,unsigned int)));
+}
+
+BrowserIntegrationReminder::~BrowserIntegrationReminder()
+{
+}
+
+void BrowserIntegrationReminder::onResourceScoresChanged(const QString &activity, const QString &client, const QString &resource, double score, unsigned int lastUpdate, unsigned int firstUpdate)
+{
+    Q_UNUSED(activity);
+    Q_UNUSED(score);
+    Q_UNUSED(lastUpdate);
+    Q_UNUSED(firstUpdate);
+    Q_UNUSED(client)
+
+    if (!resource.startsWith("applications:")) {
+        return;
+    }
+    const QString desktopFile = resource.mid(strlen("applications:"));
+    if (!m_browsers.contains(desktopFile)) {
+        return;
+    }
+    //wait a few seconds and then query if the extension is active
+    QTimer::singleShot(10 * 1000, this, [this, desktopFile]() {
+        onBrowserStarted(desktopFile);
+    });
+}
+
+void BrowserIntegrationReminder::onBrowserStarted(const QString &browser)
+{
+    QDBusConnection bus = QDBusConnection::sessionBus();
+    if (m_sni) {
+        return;
+    }
+
+    if (!KService::serviceByStorageId(browser)) {
+        return;
+    }
+
+    if (bus.interface()->isServiceRegistered(QStringLiteral("org.kde.plasma.browser_integration")) && !m_debug) {
+        //the user has the extension installed, we don't need to keep checking
+        //env var exists for easier testing
+        disableAutoload();
+        return;
+    }
+
+    m_sni = new KStatusNotifierItem(this);
+    m_shownCount++;
+    auto config = KSharedConfig::openConfig()->group("PlasmaBrowserIntegration");
+    config.writeEntry("shownCount", m_shownCount);
+
+    m_sni->setTitle(i18n("Get Plasma Browser Integration"));
+    m_sni->setIconByName("plasma-browser-integration");
+    m_sni->setStandardActionsEnabled(false);
+    m_sni->setStatus(KStatusNotifierItem::Active);
+
+    connect(m_sni, &KStatusNotifierItem::activateRequested, this, [this, browser]() {
+        const KService::Ptr service = KService::serviceByStorageId(browser);
+
+        if (!service) {
+            unload();
+            return;
+        }
+
+        KRun::runApplication(*service, QList<QUrl>() << m_browsers[browser], nullptr, KRun::RunFlags(),
+            QString(), 0);
+
+        KActivities::ResourceInstance::notifyAccessed(QUrl(QStringLiteral("applications:") + browser),
+            QStringLiteral("org.kde.plasma.browserintegrationreminder"));
+        //remove for this session.
+        //If the user installed it successfully we won't show anything next session
+        //If they didn't they'll get the link next login
+        unload();
+    });
+
+    auto *menu = new QMenu;
+    auto *action = new QAction(i18n("Do not show again"));
+    menu->addAction(action);
+    connect(action, &QAction::triggered, this, [this]() {
+        auto config = KSharedConfig::openConfig()->group("PlasmaBrowserIntegration");
+        config.writeEntry("shownCount", 100);
+        disableAutoload();
+    });
+
+    m_sni->setContextMenu(menu);
+}
+
+void BrowserIntegrationReminder::unload()
+{
+    QDBusConnection dbus = QDBusConnection::sessionBus();
+    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.kde.kded5"),
+                                                      QStringLiteral("/kded"),
+                                                      QStringLiteral("org.kde.kded5"),
+                                                      QStringLiteral("unloadModule"));
+    msg.setArguments({QVariant(QStringLiteral("browserintegrationreminder"))});
+    dbus.call(msg, QDBus::NoBlock);
+}
+
+void BrowserIntegrationReminder::disableAutoload()
+{
+    QDBusConnection dbus = QDBusConnection::sessionBus();
+    QDBusMessage msg = QDBusMessage::createMethodCall(QStringLiteral("org.kde.kded5"),
+                                                      QStringLiteral("/kded"),
+                                                      QStringLiteral("org.kde.kded5"),
+                                                      QStringLiteral("setModuleAutoloading"));
+    msg.setArguments({QVariant(QStringLiteral("browserintegrationreminder")), QVariant(false)});
+    dbus.call(msg, QDBus::NoBlock);
+    unload();
+}
+
+#include "browserintegrationreminder.moc"
---
+++ reminder/browserintegrationreminder.h
@@ -0,0 +1,70 @@
+/***************************************************************************
+ *   Copyright 2009 by Marco Martin <notmart@gmail.com>                    *
+ *   Copyright 2018 by David Edmundson <davidedmundson@kde.org>            *
+ *   Copyright 2018 by Eike Hein <hein@kde.org>                            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
+
+#pragma once
+
+#include <kdedmodule.h>
+
+#include <QObject>
+#include <QStringList>
+#include <QSet>
+#include <QUrl>
+#include <QHash>
+#include <QPointer>
+
+class KStatusNotifierItem;
+class QDBusServiceWatcher;
+
+/**
+ * This plugin's purpose is to provide the user a link to the PBI extension.
+ * It should run if the user has installed the host, but doesn't have the browser side.
+ *
+ * It creates an SNI for the first N times the user opens a browser with this setup.
+ * Then we give up
+ */
+
+class BrowserIntegrationReminder : public KDEDModule
+{
+    Q_OBJECT
+
+public:
+    BrowserIntegrationReminder(QObject *parent, const QList<QVariant>&);
+    ~BrowserIntegrationReminder() override;
+
+private Q_SLOTS:
+    void onResourceScoresChanged(const QString &activity,
+                            const QString &client,
+                            const QString &resource,
+                            double score,
+                            unsigned int lastUpdate,
+                            unsigned int firstUpdate);
+
+    void onBrowserStarted(const QString &browserName);
+
+    void unload();
+    void disableAutoload();
+
+private:
+    QHash<QString, QUrl> m_browsers;
+    QPointer<KStatusNotifierItem> m_sni;
+    bool m_debug;
+    int m_shownCount;
+};
