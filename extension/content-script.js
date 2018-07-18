216d5b12f7d58e6687e169419ea6428cad5c1369
Kai Uwe Broulik <kde@privat.broulik.de>
2018-07-18 17:27:00 +0200

    Listen for player "emptied" signal
    This is when a website reuses their player and clears its playlist when doing Ajax page navigation
    
    Differential Revision: https://phabricator.kde.org/D14209

--- extension/content-script.js
+++ extension/content-script.js
@@ -258,6 +258,13 @@ function registerPlayer(player) {
         sendPlayerInfo(player, "waiting");
     });
 
+    // playlist is now empty or being reloaded, stop player
+    // e.g. when using Ajax page navigation and the user nagivated away
+    player.addEventListener("emptied", function () {
+        // could have its own signal but for compat it's easier just to pretend to have stopped
+        sendPlayerInfo(player, "stopped");
+    });
+
     // opposite of "waiting", we finished buffering enough
     // only if we are playing, though, should we set playback state back to playing
     player.addEventListener("canplay", function () {
