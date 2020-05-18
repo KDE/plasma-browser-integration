a3f400c8589eb8ec52f3f377d96b004ffd29221c
Kai Uwe Broulik <kde@privat.broulik.de>
2020-05-18 13:56:40 +0200

    Install MutationObserver in DOMContentLoaded
    They are mostly meant to detect when a player is dynamically added/removed by AJAX when e.g.
    an overlay opens.
    I don't think it's necessary to monitor the page being constructed, potentially slowing it down.
    Especially since in DOMContentLoaded we will register all players anyway.

--- extension/content-script.js
+++ extension/content-script.js
@@ -603,11 +603,6 @@ function loadMpris() {
         });
     });
 
-    observer.observe(document, {
-        childList: true,
-        subtree: true
-    });
-
     window.addEventListener("pagehide", function () {
         // about to navigate to a different page, tell our extension that the player will be gone shortly
         // we listen for tab closed in the extension but we don't for navigating away as URL change doesn't
@@ -622,6 +617,11 @@ function loadMpris() {
 
     document.addEventListener("DOMContentLoaded", function() {
         registerAllPlayers();
+
+        observer.observe(document, {
+            childList: true,
+            subtree: true
+        });
     });
 
     document.addEventListener("fullscreenchange", () => {
