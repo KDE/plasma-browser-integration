ce3a8276db4e1f925a31fd313f6fe24b83e7b9cf
Kai Uwe Broulik <kde@privat.broulik.de>
2022-12-23 17:55:19 +0100

    Remove generateGuid function
    It's no longer needed now that all our stuff is in the page-script
    rather than facing the website

--- extension/content-script.js
+++ extension/content-script.js
@@ -18,14 +18,6 @@
 
 var callbacks = {};
 
-// from https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function generateGuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-        return v.toString(16);
-    });
-}
-
 function addCallback(subsystem, action, callback)
 {
     if (!callbacks[subsystem]) {
