70fbb2edee8ab310d5c2ba1c64eb9f9aa8962049
Kai Uwe Broulik <kde@privat.broulik.de>
2024-07-16 14:13:38 +0200

    manifest: Add back scripts
    Firefox doesn't currently support Service Workers for extensions,
    so we'll just register them both.
    
    Luckily newer Firefox and Chrome ignore each other's specifics
    so we can use both keys at once.

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -15,6 +15,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// NOTE keep in sync with manifest.json for Firefox.
 self.importScripts(
     "constants.js",
     "utils.js",
