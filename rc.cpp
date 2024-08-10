3f27939ac4d5625dd0161800caffee34877de56b
l10n daemon script <scripty@kde.org>
2024-08-10 00:43:22 +0000

    GIT_SILENT made messages (after extraction)

--- rc.cpp
+++ rc.cpp
@@ -6,3 +6,5 @@ i18nc("NAME OF TRANSLATORS","Your names");
 i18nc("EMAIL OF TRANSLATORS","Your emails");
 i18nc("NAME OF TRANSLATORS","Your names");
 i18nc("EMAIL OF TRANSLATORS","Your emails");
+i18nc("NAME OF TRANSLATORS","Your names");
+i18nc("EMAIL OF TRANSLATORS","Your emails");
